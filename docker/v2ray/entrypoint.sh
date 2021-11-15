0933ce68a6bbde14930d141f391755100051404a
shichun.fsc <shichun.fsc@alibaba-inc.com>
2021-11-15 11:39:56 +0800

    v2ray dockerbuild

---
+++ docker/v2ray/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [[ -f "$PASSWORD_FILE" ]]; then
+    PASSWORD=$(cat "$PASSWORD_FILE")
+fi
+
+if [[ -f "/var/run/secrets/$PASSWORD_SECRET" ]]; then
+    PASSWORD=$(cat "/var/run/secrets/$PASSWORD_SECRET")
+fi
+
+exec ss-server \
+      -s $SERVER_ADDR \
+      -p $SERVER_PORT \
+      -k ${PASSWORD:-$(hostname)} \
+      -m $METHOD \
+      -t $TIMEOUT \
+      -d $DNS_ADDRS \
+      --plugin ${PLUGIN} \
+      --plugin-opts ${PLUGIN_OPTS}
+      -u \
+      $ARGS
